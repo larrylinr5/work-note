d7419e61282fe5b4bd1247045fce20e611a9cfdc
larrylinr5 <larrylinr5@gmail.com>
2022-07-13 20:59:03 +0800

    新增假登入功能

--- src/components/FrontNavbar.vue
+++ src/components/FrontNavbar.vue
@@ -3,9 +3,9 @@
   <div class="row">
     <div class="col-2" @click="test">首頁</div>
     <div class="col-6 offset-4">
-      <span>帳號</span><input type="text" v-model="account">
-      <span>密碼</span><input type="text" v-model="password">
-      <button @click="test">登入</button>
+      <span>帳號</span><input type="text" v-model="account" :disabled="loginFlg">
+      <span>密碼</span><input type="password" v-model="password" :disabled="loginFlg">
+      <button @click="login">登入</button>
       <button @click="logOut">登出</button>
     </div>
   </div>
@@ -13,32 +13,35 @@
 </template>
 
 <script>
-import emitter from '@/libs/emitter'
+// import emitter from '@/libs/emitter'
 
 export default {
   data () {
     return {
       account: '',
-      password: ''
+      password: '',
+      loginFlg: false
     }
   },
   components: {
   },
   methods: {
-    test () {
-      this.$router.push('/')
+    login () {
+      if (this.account === 'M30349' && this.password === 'test') {
+        this.loginFlg = true
+      } else {
+        alert('帳號或密碼輸入錯誤')
+      }
+    // this.$router.push('/')
     },
     logOut () {
       this.account = ''
       this.password = ''
+      this.loginFlg = false
       alert('登出')
     }
   },
   mounted () {
-    this.getCart()
-    emitter.on('get-cart', () => {
-      this.getCart()
-    })
   }
 }
 </script>
